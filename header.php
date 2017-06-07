2c3496778d0f403beaf6f5307f384ad63e76d54b
Benjamin Harris <Bigben83@users.noreply.github.com>
2017-06-07 11:52:00 +1000

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<div class="sidenav">
+	<!-- Sidenav links here -->
+</div>
+
+	  
+<div id="map-header">
+	<div id="navMain">
+		<div class="menu">
+			<ul>
+				<li class="first"><a href="#" title="Home">Website</a></li>
+				<li><a href="#" title="CRM">CRM</a></li>
+				<li><a href="#p" title="Dashboard">Phone Log</a></li>
+				<li><a href="#" title="Dashboard">Dashboard</a></li>
+				<li><a href="./voicemail/index.php" title="Dashboard">Voicemail</a></li>
+			</ul>
+		</div>
+		<div class="phone"><p><a href="#"><b>01 1234 5678</b></a></p></div> 
+	</div>
+</div>
